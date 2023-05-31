c63cc04235f62e57072198eee4df28bf29a38559
Intellbg <nicolas.rnbp@gmail.com>
2023-05-31 17:28:41 -0500

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({site: 'https://intellbg.github.io/horus_webpage',});
